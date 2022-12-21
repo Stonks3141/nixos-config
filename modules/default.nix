b1f666bb79d1e4ec7382eb566bc4919b402d2abe
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-12-21 10:58:53 -0500

    set XDG environment variables

--- modules/default.nix
+++ modules/default.nix
@@ -11,8 +11,30 @@
     example = "22.11";
   };
 
+  config.environment.variables = {
+    XDG_CURRENT_DESKTOP = "sway";
+    XDG_SESSION_TYPE = "wayland";
+  };
+
+  config.environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "sway";
+    XDG_SESSION_TYPE = "wayland";
+  };
+
   config.home-manager.users.samn = { pkgs, ... }: {
     home.stateVersion = config.samn.stateVersion;
+    home.sessionVariables = {
+      EDITOR = "hx";
+      BROWSER = "firefox";
+      TERMINAL = "kitty";
+      XDG_DOWNLOADS_DIR = "~/Downloads";
+      XDG_CURRENT_DESKTOP = "sway";
+      XDG_SESSION_TYPE = "wayland";
+    };
+    systemd.user.sessionVariables = {
+      XDG_CURRENT_DESKTOP = "sway";
+      XDG_SESSION_TYPE = "wayland";
+    };
     nixpkgs.config = {
       allowUnfree = true;
       packageOverrides = pkgs: {
