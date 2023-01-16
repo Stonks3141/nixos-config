7678345e0560ba0a1871960210a73d097edf357c
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-01-16 12:31:00 -0500

    Add theme for bat

--- modules/applications/default.nix
+++ modules/applications/default.nix
@@ -12,7 +12,6 @@ in
 
       # CLIs
       ripgrep
-      bat
       bottom
       zoxide
       fd
@@ -118,6 +117,17 @@ in
           branch_fg: Rgb(139, 213, 202)
         )
       '';
+    };
+
+    programs.bat = {
+      enable = true;
+      config.theme = "Catppuccin-macchiato";
+      themes."Catppuccin-macchiato" = builtins.readFile (pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "bat";
+        rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+        sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+      } + "/Catppuccin-macchiato.tmTheme");
     };
 
     programs.gpg = {
