66e2bcdc8957afdaba7d2eedb579af28a6a46a73
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-01-20 21:41:18 -0500

    run formatter

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -3,5 +3,5 @@
 
   age.secrets."passwords/users/samn".file = ../secrets/passwords/users/samn.age;
   age.secrets."networks".file = ../secrets/networks.age;
-  #age.secrets."wireguard/pavilion.key".file = ../secrets/wireguard/pavilion.key.age;
+  # age.secrets."wireguard/pavilion.key".file = ../secrets/wireguard/pavilion.key.age;
 }
