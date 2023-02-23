e5d10caf33527447087daf9421f244c86fc610b8
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-22 21:38:24 -0500

    set up git-send-email

--- modules/applications/default.nix
+++ modules/applications/default.nix
@@ -51,10 +51,17 @@
       FZF_DEFAULT_OPTS = "--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796";
     };
 
-    programs.git = {
+    programs.git = rec {
       enable = true;
+      package = pkgs.gitFull;
       userEmail = "samuel.l.nystrom@gmail.com";
       userName = "Sam Nystrom";
+      extraConfig.sendemail = {
+        smtpserver = "smtp.gmail.com";
+        smtpuser = userEmail;
+        smtpencryption = "tls";
+        smtpserverport = 587;
+      };
     };
   };
 }
