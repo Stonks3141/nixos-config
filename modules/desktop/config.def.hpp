957a1b97ebe9f930985066e3cd30eb6a02890a2f
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-03-03 21:39:59 -0500

    configure somebar with file instead of patch

---
+++ modules/desktop/config.def.hpp
@@ -0,0 +1,27 @@
+// somebar - dwl bar
+// See LICENSE file for copyright and license details.
+
+#pragma once
+#include "common.hpp"
+
+constexpr bool topbar = true;
+
+constexpr int paddingX = 10;
+constexpr int paddingY = 3;
+
+// See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
+constexpr const char* font = "FiraCode Nerd Font 12";
+
+constexpr ColorScheme colorInactive = {Color(0xca, 0xd3, 0xf5), Color(0x24, 0x27, 0x3a)};
+constexpr ColorScheme colorActive = {Color(0xca, 0xd3, 0xf5), Color(0xed, 0x87, 0x96)};
+constexpr const char* termcmd[] = {"foot", nullptr};
+
+static std::vector<std::string> tagNames = {
+	"1", "2", "3",
+	"4", "5", "6",
+	"7", "8", "9",
+};
+
+constexpr Button buttons[] = {
+	{ ClkStatusText,   BTN_RIGHT,  spawn,      {.v = termcmd} },
+};
--- modules/desktop/dwl.nix
+++ modules/desktop/dwl.nix
@@ -4,9 +4,9 @@ let
     path = ./wallpaper.jpg;
     name = "wallpaper";
   };
-  somebar = pkgs.somebar.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [ ../../patches/somebar.patch ];
-  });
+  somebar = pkgs.somebar.override {
+    conf = ./config.def.hpp;
+  };
   status = pkgs.writeShellApplication {
     name = "status.sh";
     runtimeInputs = with pkgs; [ pulseaudio brightnessctl ];
@@ -22,23 +22,9 @@ let
   '';
   dwl = pkgs.dwl.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
-      # pkgs.fetchurl {
-      #   url = "https://git.sr.ht/~stonks3141/dwl/commit/a21980497c9cfe195b79295c70971933ae3a3601.patch";
-      #   sha256 = "";
-      # }
       ../../patches/gaps.patch
-      # pkgs.fetchurl {
-      #   url = "https://git.sr.ht/~stonks3141/dwl/commit/212ed89366f45f43248b054faa474ab9c9928307.patch";
-      #   sha256 = "";
-      # }
       ../../patches/config.patch
     ];
-    # src = pkgs.fetchFromSourcehut {
-    #   owner = "~stonks3141";
-    #   repo = "dwl";
-    #   rev = "e4d67b2f1e2dcd013d2d3d9d3c475db98e28d611";
-    #   sha256 = "sha256-IZ3k+9pXIJKRHoWeLyOX9GG+iJkUweykps/QeYKCtIU=";
-    # };
   });
 in
 {
