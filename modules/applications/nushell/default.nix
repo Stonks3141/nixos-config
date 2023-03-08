24ca29520f2cd84707898d10a6abb635d38a2940
Sam Nystrom <sam@samnystrom.dev>
2023-03-07 19:29:46 -0500

    set sessionVariables in nushell config

--- modules/applications/nushell/default.nix
+++ modules/applications/nushell/default.nix
@@ -1,12 +1,18 @@
 # SPDX-FileCopyrightText: 2022 Sam Nystrom <sam@samnystrom.dev>
 # SPDX-License-Identifier: CC0-1.0
 
-{ ... }: {
+{ config, ... }: {
   home-manager.users.samn = {
     programs.nushell = {
       enable = true;
       configFile.source = ./config.nu;
       envFile.source = ./env.nu;
+      extraEnv = with builtins; concatStringsSep
+        "\n"
+        (attrValues
+          (mapAttrs
+            (name: value: "let-env ${name} = '${value}'")
+            config.home-manager.users.samn.home.sessionVariables));
     };
   };
 }
